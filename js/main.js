2c10153973f4344732d6b05e1cdbb4f3a50dbce8
vasilevski85@gmail.com <vasilevski85@gmail.com>
2019-03-31 12:43:43 +0200

    added complete.html

--- js/main.js
+++ js/main.js
@@ -9,3 +9,44 @@ $(".fas").click(function(event){
 	 	$(this).siblings(".answer").hide();
 	 }
 });
+
+//Info form click next listener
+$("#input-info-submit").click(function(event){
+	validateInfoForm();
+});
+
+//Payment form click purchase listener
+$("#input-payment-purchase").click(function(event){
+	event.preventDefault();
+	validatePaymentForm();
+});
+
+function validateInfoForm(){
+	var firstName = $("#input-info-firstname").val();
+	var lastName = $("#input-info-lastname").val();
+	var lawOfSchool = $("#input-info-lawofschool").val();
+	var email = $("#input-info-email").val();
+	var password = $("#input-info-password").val();
+
+	if (validateInput(firstName, "First Name") && validateInput(lastName, "Last Name") &&
+		validateInput(email, "Email") && validateInput(password, "Password")) {
+		location = "payment.html";
+	}
+}
+
+function validatePaymentForm(){
+	var cardName = $("#input-payment-cardname").val();
+	var cardNumber = $("#input-payment-cardnumber").val();
+	var cardExpMonth = $("#select-payment-month").val();
+	var cardExpYear = $("#select-payment-year").val();
+
+	location = "complete.html";
+}
+
+function validateInput(input, field){
+	if (input.length == 0) {
+		alert("You must enter " + field);
+	}else {
+		return true;
+	}
+}
